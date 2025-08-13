99ccc0641d2f30dbd0adca652bc687892447e21b
vivekk-snippyly <vivek@snippyly.com>
2025-08-13 19:15:13 +0530

    Fixing build error.

--- src/components/BlockNoteEditor.tsx
+++ src/components/BlockNoteEditor.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useEffect, useMemo, useRef, useState } from "react";
 import { useCreateBlockNote } from "@blocknote/react";
